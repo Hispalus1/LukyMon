19d73305dfa4f3720aa1ab08017b9ff12baf8492
Lukáš Hrňa <77784613+Hispalus1@users.noreply.github.com>
2023-05-14 14:14:18 +0200

    Add files via upload

---
+++ Accessory/Boots/GelBoots.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using LukyMon;
+
+
+
+namespace LukyMon.Accessory.Boots
+{
+    public class GelBoots : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sticky Boots");
+            Tooltip.SetDefault("Try to run like slime!!");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.value = 0;
+            Item.rare = ItemRarityID.Gray;
+            Item.accessory = true;
+        }
+        
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.moveSpeed += 0.3f;
+            player.accRunSpeed += 0.2f;
+            player.jumpBoost = true;
+            player.GetModPlayer<WoodenSlime>().stickyShoes = true;
+        }
+
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Gel, 25);
+            recipe.AddTile(TileID.Anvils);
+            recipe.HasResult(this);
+            recipe.Register();
+        }
+    }
+}
--- Items/GelShotGun/GelShotGun.cs
+++ Items/GelShotGun/GelShotGun.cs
@@ -7,34 +7,34 @@ using LukyMon.Projektile.GelBullet;
 using System;
 
 
-namespace LukyMon.Items.GelGun
+namespace LukyMon.Items.GelShotGun
 {
-    public class GelGun : ModItem
+    public class GelShotGun : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Gel Revolver"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            DisplayName.SetDefault("Gel Shotgun"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
             Tooltip.SetDefault("Shoot gel projectiles");
         }
 
         public override void SetDefaults()
         {
-            Item.damage = 9;
+            Item.damage = 6;
             Item.DamageType = DamageClass.Ranged;
             Item.width = 40;
             Item.height = 40;
-            Item.useTime = 20;
-            Item.useAnimation = 20;
+            Item.useTime = 40;
+            Item.useAnimation = 40;
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.knockBack = 0.6f;
+            Item.knockBack = 3.5f;
             Item.value = 2500;
             Item.rare = ItemRarityID.Blue;
             Item.UseSound = SoundID.Item11;
-            Item.autoReuse = true;
             Item.shoot = 1;
             Item.useAmmo = AmmoID.Bullet;
             Item.shootSpeed = 5f;
             Item.noMelee = true;
+            Item.scale = 0.1f;
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
@@ -45,16 +45,18 @@ namespace LukyMon.Items.GelGun
             {
                 type = ModContent.ProjectileType<GelBullet>();
             }
-            Projectile.NewProjectile(source, position, velocity, type, damage, (int)knockback, player.whoAmI);
+            for (var i = 0; i < Main.rand.Next(3, 4); i++)
+            {
+                Vector2 perturbedSpeed = velocity.RotatedByRandom(MathHelper.ToRadians(25));
+                Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+            }
             return false;
         }
-
         public override Vector2? HoldoutOffset()
         {
             Vector2 offset = new Vector2(6, 0);
             return offset;
         }
-
 
 
         public override void AddRecipes()
