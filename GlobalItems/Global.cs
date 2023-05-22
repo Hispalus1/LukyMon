76bb9e66cee8302992b404f28ed224fa71fce0ba
Lukáš Hrňa <77784613+Hispalus1@users.noreply.github.com>
2023-05-22 18:00:55 +0200

    Update Global.cs

--- GlobalItems/Global.cs
+++ GlobalItems/Global.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-
+//global item
 namespace LukyMon.Global
 {
 	public class GlobalItemList : GlobalItem
